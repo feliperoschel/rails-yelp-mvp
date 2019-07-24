be24436f2bf89875dd6bd62c866fac68b182d141
Felipe Roschel <f.roschel@gmail.com>
2019-07-24 23:05:39 +0800

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning up 'restaurant' table..."
+Restaurant.destroy_all if Rails.env.development?
+
+puts "Creating new restaurants..."
+category = ["chinese", "italian", "japanese", "french", "belgian"]
+
+10.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: category.sample
+  )
+  5.times do
+    restaurant.reviews.new(
+      content: Faker::Restaurant.review,
+      rating: rand(0..5)
+    )
+  end
+  restaurant.save
+end
+
+puts "Created #{Restaurant.count} restaurants"
